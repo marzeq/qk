a551cc05792425b3ddfea9f73fe28130d8925131
Antoni Marzec <marzeqmarzeq@gmail.com>
2026-07-30 21:59:11 +0200

    .

--- codegen/llvmbackend/link_dynamic.go
+++ codegen/llvmbackend/link_dynamic.go
@@ -4,7 +4,5 @@ package llvmbackend
 
 /*
 #cgo LDFLAGS: -lclang-cpp -llldELF -llldCOFF -llldMinGW -llldMachO -llldCommon -lLLVM
-#cgo !darwin LDFLAGS: -lstdc++
-#cgo darwin LDFLAGS: -lc++
 */
 import "C"
