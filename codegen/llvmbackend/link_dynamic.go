0ea11f1b1444ea347a15a22973ec74b7ce37c9ec
Antoni Marzec <marzeqmarzeq@gmail.com>
2026-08-13 14:56:32 +0200

    fix msys building with mingw

--- codegen/llvmbackend/link_dynamic.go
+++ codegen/llvmbackend/link_dynamic.go
@@ -3,7 +3,9 @@
 package llvmbackend
 
 /*
-#cgo LDFLAGS: -lclang-cpp -llldELF -llldCOFF -llldMinGW -llldMachO -llldWasm -llldCommon -lLLVM
+#cgo LDFLAGS: -lclang-cpp -llldELF -llldCOFF -llldMinGW -llldMachO -llldWasm -llldCommon
+#cgo !windows LDFLAGS: -lLLVM
+#cgo windows LDFLAGS: -lLLVM-22 -lz -lzstd
 #cgo linux LDFLAGS: -lz -lzstd
 */
 import "C"
